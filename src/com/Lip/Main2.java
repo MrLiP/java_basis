6ef64944508f4d994dc1178136c1c2b2f232d136
MrLiP <563040043@qq.com>
2021-08-21 11:10:50 +0800

    Test

--- src/com/Lip/Main2.java
+++ src/com/Lip/Main2.java
@@ -1,37 +1,57 @@
 package com.Lip;
 
-import java.awt.*;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main2 {
 
+    static int ans = 0;
+
     public static void main(String[] args) {
-        long l = 499;
-
-
-    }
-
-    public static Integer[] solution(int[] a, int[] b) {
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        int l = 0, r = 0;
-
-        while (l < a.length && r < b.length) {
-            if (a[l] == b[r]) {
-                ans.add(a[l]);
-                l++;
-                r++;
-            } else if (a[l] > b[r]) {
-                r++;
-            } else {
-                l++;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] nums = new int[n][n];
+        int zeros = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String temp = sc.next();
+                if (temp.equals(".")) {
+                    nums[i][j] = 0;
+                    zeros++;
+                }
+                else nums[i][j] = 1;
             }
         }
 
-        return ans.toArray(new Integer[0]);
+        dfs(nums, n, 0, 0, zeros);
+
+        System.out.println(ans);
     }
 
+    private static void dfs(int[][] nums, int n, int row, int col, int zeros) {
+        if (nums[row][col] == 1) return;
+
+        nums[row][col] = 1;
+        zeros--;
+
+        if (zeros == 0) {
+            ans++;
+            return;
+        }
+
+        if (row - 1 >= 0) dfs(nums, n, row - 1, col, zeros);
+        if (row + 1 < n) dfs(nums, n, row + 1, col, zeros);
+        if (col - 1 >= 0) dfs(nums, n, row, col - 1, zeros);
+        if (col + 1 < n) dfs(nums, n, row, col + 1, zeros);
+        nums[row][col] = 0;
+    }
+
+    private static boolean check(int[][] nums) {
+        int n = nums.length;
+        for (int[] num : nums) {
+            for (int j = 0; j < n; j++) {
+                if (num[j] == 0) return false;
+            }
+        }
+        return true;
+    }
 }
