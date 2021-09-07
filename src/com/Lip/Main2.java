746c620ff0eb8e116d2e920acc8231c9b36a74b0
MrLiP <563040043@qq.com>
2021-09-07 18:46:41 +0800

    Test

--- src/com/Lip/Main2.java
+++ src/com/Lip/Main2.java
@@ -4,54 +4,73 @@ import java.util.*;
 
 public class Main2 {
 
-    static int ans = 0;
+    static long ans = 0;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] nums = new int[n][n];
-        int zeros = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                String temp = sc.next();
-                if (temp.equals(".")) {
-                    nums[i][j] = 0;
-                    zeros++;
-                }
-                else nums[i][j] = 1;
+
+
+        System.out.println(GetNumOfExpress("1^0|0|1", false));
+    }
+
+
+    /**
+     * Note: 类名、方法名、参数名已经指定，请勿修改
+     *
+     *
+     * 计算express结果值满足desired的组合种类数
+     * @param express string字符串 express字符串
+     * @param desired bool布尔型 预期结果
+     * @return long长整型
+     */
+    public static long GetNumOfExpress(String express, boolean desired) {
+        // write code here
+
+        //dfs(express, desired);
+
+        char[] chs = express.toCharArray();
+
+        //int[][] t = new int[chs.length][chs.length];
+        //int[][] f = new int[chs.length][chs.length];
+
+
+
+        return dfs(chs, 0, chs.length - 1, desired);
+    }
+
+    private static int dfs(char[] chs, int start, int end, boolean desired){
+        //递归结束条件
+        if(start == end){
+            if((chs[start] == '1' && desired) ||  (chs[start] == '0' && !desired)){
+                return 1;
+            }else {
+                return 0;
             }
         }
 
-        dfs(nums, n, 0, 0, zeros);
+        int count = 0;
 
-        System.out.println(ans);
-    }
-
-    private static void dfs(int[][] nums, int n, int row, int col, int zeros) {
-        if (nums[row][col] == 1) return;
-
-        nums[row][col] = 1;
-        zeros--;
-
-        if (row == n - 1 && zeros == 0) {
-            ans++;
-            return;
-        }
-
-        if (row - 1 >= 0) dfs(nums, n, row - 1, col, zeros);
-        if (row + 1 < n) dfs(nums, n, row + 1, col, zeros);
-        if (col - 1 >= 0) dfs(nums, n, row, col - 1, zeros);
-        if (col + 1 < n) dfs(nums, n, row, col + 1, zeros);
-        nums[row][col] = 0;
-    }
-
-    private static boolean check(int[][] nums) {
-        int n = nums.length;
-        for (int[] num : nums) {
-            for (int j = 0; j < n; j++) {
-                if (num[j] == 0) return false;
+        for(int i = start+1; i < end; i += 2){
+            if(chs[i] == '&' && desired){
+                count += dfs(chs,start,i-1,true) * dfs(chs,i+1,end,true);
+            }else if((chs[i] == '&' && !desired)){
+                count += dfs(chs,start,i-1,true) * dfs(chs,i+1,end,false)
+                        + dfs(chs,start,i-1,false) * dfs(chs,i+1,end,true)
+                        + dfs(chs,start,i-1,false) * dfs(chs,i+1,end,false);
+            }else if((chs[i] == '|' && desired)){
+                count += dfs(chs,start,i-1,true) * dfs(chs,i+1,end,false)
+                        + dfs(chs,start,i-1,false) * dfs(chs,i+1,end,true)
+                        + dfs(chs,start,i-1,true) * dfs(chs,i+1,end,true);
+            }else if(chs[i] == '|' && !desired){
+                count += dfs(chs,start,i-1,false) * dfs(chs,i+1,end,false);
+            }else if(chs[i] == '^' && desired){
+                count += dfs(chs,start,i-1,true) * dfs(chs,i+1,end,false)
+                        + dfs(chs,start,i-1,false) * dfs(chs,i+1,end,true);
+            }
+            else if(chs[i] == '^' && !desired){
+                count += dfs(chs,start,i-1,true) * dfs(chs,i+1,end,true)
+                        + dfs(chs,start,i-1,false) * dfs(chs,i+1,end,false);
             }
         }
-        return true;
+        return count;
     }
 }
---
+++ src/com/Lip/recruitment/BaiDu.java
@@ -0,0 +1,12 @@
+package com.Lip.recruitment;
+
+import com.Lip.TreeNode;
+
+import java.util.*;
+
+public class BaiDu {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
