479617dd67b5264a5781840cb319e6d82b71bee1
MrLiP <563040043@qq.com>
2021-09-13 18:46:12 +0800

    Test

---
+++ src/com/Lip/recruitment/Bilibili.java
@@ -0,0 +1,14 @@
+package com.Lip.recruitment;
+
+import java.util.*;
+
+public class Bilibili {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+
+
+}
