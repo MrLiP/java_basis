bc03a0be198a75cd0cecb19bbf4b6b460ac3dd28
MrLiP <563040043@qq.com>
2021-09-16 18:52:22 +0800

    Test

---
+++ src/com/Lip/cls/TengXunMusic.java
@@ -0,0 +1,15 @@
+package com.Lip.cls;
+
+import java.util.*;
+
+public class TengXunMusic {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println();
+    }
+
+
+}
