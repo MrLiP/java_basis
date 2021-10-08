c7efd11f41f6ffce24a730834ac2b64f90523374
MrLiP <563040043@qq.com>
2021-10-08 10:40:52 +0800

    Test

--- src/com/Lip/recruitment/HuaWei.java
+++ src/com/Lip/recruitment/HuaWei.java
@@ -4,53 +4,96 @@ import java.util.Scanner;
 
 public class HuaWei {
 
-    private static int solution_1(String text1, String text2) {
-        int[][]dp=new int[text1.length()+1][text2.length()+1];
-        for(int i=1;i<=text1.length();i++){
-            for(int j=1;j<=text2.length();j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[text1.length()][text2.length()];
-    }
-
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // please define the JAVA input here. For example: Scanner s = new Scanner(System.in);
+        // please finish the function body here.
+        // please define the JAVA output here. For example: System.out.println(s.nextInt());
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//
+//        int[] nums = new int[t];
+//
+//        for (int i = 0; i < t; i++) {
+//            nums[i] = sc.nextInt();
+//        }
+//
+//        System.out.println(solution_3(nums, t));
 
-        //List<Integer> list = new ArrayList<Integer>;
-
-        int[] nums = new int[10];
-        for(int i = 0; i < 10; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        nums = solution_2(nums);
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print(nums[i] + " ");
-        }
+        System.out.println(solution_1("0002:0000:0000:0000:0000:0100:0000:0000"));
     }
 
-    private static int[] solution_2(int[] nums) {
-        int p1 = -1, p2 = -1;
+    private static String solution_1(String str) {
+        if (str.length() != 39) {
+            return "error";
+        }
+        StringBuilder sb = new StringBuilder();
+        char[] chs = str.toCharArray();
 
-        for ( int i = 0; i < nums.length; i++) {
-            if (nums[i] < 3) {
-                p2++;
-                swap(i, p2, nums);
+        int n = 0;
+        boolean flag = true;
+        boolean fir = false;
+        for (int i = 0; i < chs.length; i++) {
+            if (i % 5 == 0) fir = true;
+            else fir = false;
 
-                if (nums[p2] < 2) {
-                    p1++;
-                    swap(p1, p2, nums);
+            //if (fir && chs[i] != '0') {
+            //    while (chs[i] != ':') {
+            //        sb.append(chs[i++]);
+            //    }
+            //    sb.append(chs[i++]);
+            //}
+            if (fir && chs[i] == '0') {
+                if (flag) {
+                    int k = 0;
+                    while (i < chs.length - 1 && (chs[i] == '0' || chs[i] == ':')) {
+                        k++;
+                        i++;
+                    }
+                    if (k >= 10) {
+                        flag = false;
+                        i -= k;
+                        i += (k / 5 * 5) - 1;
+                    } else if (k >= 5) {
+                        sb.append("0");
+                        i -= k;
+                        i += (k / 5 * 5) - 1;
+                    }
+                } else {
+                    while (i < chs.length - 1 && chs[i] == '0') {
+                        i++;
+                    }
+                    if (chs[i] == ':') sb.append("0");
                 }
             }
+
+            sb.append(chs[i]);
         }
-        return nums;
+
+        return sb.toString();
+    }
+
+    private static long solution_3(int[] nums, int t) {
+        long ans = 0;
+        int[][][] dp = new int[t][t][2];
+        // 0 store max , 1 store min;
+
+        for (int i = 0; i < t; i++) {
+            dp[i][i][0] = nums[i];
+            dp[i][i][1] = nums[i];
+        }
+
+        for (int i = 0; i < t - 1; i++) {
+            for (int j = i + 1; j < t; j++) {
+                dp[i][j][0] = Math.max(dp[i][j - 1][0], nums[j]);
+                dp[i][j][1] = Math.min(dp[i][j - 1][1], nums[j]);
+
+                ans += (dp[i][j][0] - dp[i][j][1]) * (j - i + 1);
+                ans %= 1000000007;
+            }
+        }
+
+        return ans;
     }
 
     private static void swap(int a, int b, int[] nums) {
